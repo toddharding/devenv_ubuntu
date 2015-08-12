c2ef98a4475814b13eee2888e3ad3e3c70239fc1
Todd Harding <t.harding@dundee.ac.uk>
2015-08-12 09:24:41 +0100

    edited readme, added install all to bootstrap.sh

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # devenv_ubuntu
+curl -s https://raw.githubusercontent.com/toddharding/devenv_ubuntu/master/bootstrap.sh | bash
+
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,16 +1,27 @@
 #!/bin/bash
 
-# This set of scripts is for setting up my development environment on Ubuntu
+# This set of scripts is for setting up my development environment on Ubuntu from a fresh install
+
+START_DIR=pwd
+INSTALL_DIR=~/devenv_scripts/
+
 sudo apt-get install git
 
 echo "Ubuntu Development Environment Setup Scripts"
 echo ""
-echo "Please enter your email"
+echo -n "Please enter your email: "
 read email
 
-echo "Please enter your name"
+echo -n "Please enter your name: "
 read name
 
 git config --global user.email "$email"
 git config --global user.name "$name"
-git clone https://github.com/toddharding/devenv_ubuntu ~/devenv_scripts/ 
+
+git clone https://github.com/toddharding/devenv_ubuntu $INSTALL_DIR
+
+cd $INSTALL_DIR
+source install_all.sh
+cd $START_DIR
+
+ 
