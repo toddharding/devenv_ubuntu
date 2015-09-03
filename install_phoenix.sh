8d218c52eae43aaa9e51b13cec7d5e6fd8048826
Todd Harding <t.harding@dundee.ac.uk>
2015-09-03 16:06:09 +0100

    updated phoenix install version to 1.0

--- install_phoenix.sh
+++ install_phoenix.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 yes | mix local.hex
-yes | mix archive.install https://github.com/phoenixframework/phoenix/releases/download/v0.16.1/phoenix_new-0.16.1.ez
+yes | mix archive.install https://github.com/phoenixframework/phoenix/releases/download/v1.0.0/phoenix_new-1.0.0.ez
