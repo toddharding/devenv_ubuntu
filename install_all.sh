fd3916422f60769524fed41d3be58c302a875c70
Todd Harding <tharding@findmypast.com>
2017-02-28 17:17:19 +0000

    some changes for new config

--- install_all.sh
+++ install_all.sh
@@ -4,12 +4,12 @@ source install_atom.sh
 source install_erlang.sh
 source install_elixir.sh
 source install_node.sh
-source install_phoenix.sh
-source install_fish.sh
-source install_postgres.sh
+#source install_phoenix.sh
+#source install_fish.sh
+#source install_postgres.sh
 
 source setup_nautilus.sh
 source setup_unity.sh
 
-sudo apt-get install -y shutter guake
+#sudo apt-get install -y shutter guake
 sudo apt-get install -y ttf-mscorefonts-installer
